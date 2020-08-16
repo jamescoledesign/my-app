f6def53aae92dc8dc5363a9080f169aa8113a09d
James Cole <james@openstack.org>
2020-08-16 14:05:35 -0500

    Basic styling with css

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Learn to design and code React apps</h1>
-    <p>Complete course about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <Link to="/page-2/">Watch the video</Link> <br />
+    <div className="hero">
+      <div className="heroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete course about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <Link to="/page-2/">Watch the video</Link> 
+      </div>
+    </div>
   </Layout>
 )
 
