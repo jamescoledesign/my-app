70bff93113dd16477367a6b8544729fc53183f0b
James Cole <james@openstack.org>
2020-08-16 13:34:32 -0500

    Match tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Learn to design and code React apps</h1>
     <p>Complete course about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Link to="/page-2/">Watch the video</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
