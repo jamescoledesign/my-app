f9a30d50137367eb19a6d856f7bc4a246bc02c4e
James Cole <james@openstack.org>
2020-08-18 16:41:35 -0500

    Add nav states and events

--- src/components/header.js
+++ src/components/header.js
@@ -3,16 +3,42 @@ import PropTypes from "prop-types"
 import React from "react"
 import './header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="header">
-    <div className="headerGroup">
-      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30px" /></Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy"><button>Buy</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    } 
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'header headerScrolled' : 'header'}>
+        <div className="headerGroup">
+          <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30px" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy"><button>Buy</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
