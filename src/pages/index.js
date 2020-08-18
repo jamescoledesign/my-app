1a722054f0ce55884a782ca7c4b180431810cd3f
James Cole <james@openstack.org>
2020-08-17 21:35:23 -0500

    Add components and props

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Card from '../components/card'
 
 const IndexPage = () => (
   <Layout>
@@ -35,6 +36,27 @@ const IndexPage = () => (
           />
           </path>
         </svg>
+      </div>
+        <div className="Cards">
+          <h2>11 courses, more coming</h2>
+          <div className="cardGroup">
+          <Card 
+          title="Design System" 
+          text="10 sections" 
+          image={require('../images/wallpaper.jpg')} />
+          <Card 
+          title="React for Designers" 
+          text="12 sections" 
+          image={require('../images/wallpaper2.jpg')} />
+          <Card 
+          title="Sound Design" 
+          text="5 sections" 
+          image={require('../images/wallpaper3.jpg')} />
+          <Card 
+          title="ARKit 2" 
+          text="10 sections" 
+          image={require('../images/wallpaper4.jpg')} />
+        </div>
       </div>
     </div>
   </Layout>
